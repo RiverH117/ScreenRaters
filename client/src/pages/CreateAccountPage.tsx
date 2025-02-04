7743215c5747c9f12107b5fb963c7eb0461bf593
bowman-crego <bowmanccrego@gmail.com>
2025-02-03 21:21:11 -0800

    added a create account page

--- client/src/pages/CreateAccountPage.tsx
+++ client/src/pages/CreateAccountPage.tsx
@@ -1,27 +1,34 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import bcrypt from "bcryptjs";
 
 const CreateAccountPage: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userData = { username, password: hashedPassword };
+    localStorage.setItem(username, JSON.stringify(userData));
     // Handle account creation logic here
     navigate("/login");
   };
 
   return (
-    <div>
-      <NavBar />
-      <div>
-        <h1>Create Account</h1>
-        <form onSubmit={handleSubmit}>
+    <div className="w-full max-w-xs">
+      <div className="flex justify-center mt-20">
+        <form
+          className="bg-gray shadow-md rounded px-8 py-8 pt-8 mb-4"
+          onSubmit={handleSubmit}
+        >
+          <h1>Create Account</h1>
           <label>
             Username:
             <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="email"
               name="email"
               value={username}
@@ -31,13 +38,19 @@ const CreateAccountPage: React.FC = () => {
           <label>
             Password:
             <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="password"
               name="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </label>
-          <button type="submit">Create Account</button>
+          <button
+            className="bg-gray-500 hover:bg-gray-300 rounded font-bold py-2 px-4 mt-4"
+            type="submit"
+          >
+            Create Account
+          </button>
         </form>
       </div>
     </div>
