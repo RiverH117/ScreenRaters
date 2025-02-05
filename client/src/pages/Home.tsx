45e2a7ed1816e7a6eaec99d75fb68bc14a32562b
Christopher Marinica <chris.marinica@gmail.com>
2025-02-04 21:07:56 -0700

    added .env and movies.tsx issue fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Header from "../components/Header";
-
+//import { Link } from "react-router-dom";
+//import Header from "../components/Header";
+import Movies from "../components/Movies";
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to ScreenRaters</h1>
       <p>Your go-to platform for movie and TV show ratings.</p>
+      <Movies />
     </div>
   );
 };
